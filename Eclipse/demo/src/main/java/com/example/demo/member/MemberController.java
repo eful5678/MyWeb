ce0ffd70b5b5ee9c7359165be0c84a21fb7d6bcb
eful5678 <77581778+eful5678@users.noreply.github.com>
2021-03-15 17:45:37 +0900

    layout

--- Eclipse/demo/src/main/java/com/example/demo/member/MemberController.java
+++ Eclipse/demo/src/main/java/com/example/demo/member/MemberController.java
@@ -1,10 +1,14 @@
 package com.example.demo.member;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MemberController {
 	
-	
+	@RequestMapping("/")
+	public String main() {
+		return "/member/main";	
+	}
 
 }
---
+++ Eclipse/demo/src/main/webapp/WEB-INF/views/member/join.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=EUC-KR"
+    pageEncoding="EUC-KR"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="EUC-KR">
+<title>Login Page</title>
+</head>
+<body>
+<header>
+<a href="">Main</a>
+</header>
+<h1>Login</h1>
+</body>
+</html>
